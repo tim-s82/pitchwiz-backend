208d0dccbb4e1ccecf3a07ef5aeaca39b4b1e187
Tim Shapcott <tim.shapcott@gmail.com>
2026-08-20 16:54:57 +0100

    feat: configure development settings and project dependencies for database and host management

--- base/settings/dev.py
+++ base/settings/dev.py
@@ -4,7 +4,7 @@ import dj_database_url
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-qurch#-+_$5_b^fwcvxcm^tplr@l6bm1t8ew^bj7*7b!yd&_gz"
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -20,4 +20,3 @@ DATABASES = {
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
-    
