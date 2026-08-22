aec08e168d87e60b59ad437f9d8eec541c73804b
Tim Shapcott <tim.shapcott@gmail.com>
2026-08-22 14:13:44 +0100

    feat: implement partitioned Django settings with common and local configurations

--- base/settings/local.py
+++ base/settings/local.py
@@ -12,3 +12,12 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
+print(f"SQLite DB: {BASE_DIR / 'db.sqlite3'}")
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "http://localhost:3000",
+    "http://127.0.0.1:5173",
+    "http://127.0.0.1:3000",
+]
